e595e4ca7c7c1342be12c929565cd14d39eedcc9
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-01 18:30:25 +0300

    Add minimal test case program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace actions_test_casharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, World!"));
+        }
+    }
+}
